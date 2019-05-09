e346ab23b3b92c2e296ea938c1cfeae819d375ce
Andrii Kostenko <andrey@kostenko.name>
2019-05-09 22:17:39 +0300

    Immutable plugin

--- src/stringify.js
+++ src/stringify.js
@@ -1,12 +1,13 @@
 const prettyFormat = require('pretty-format');
 const ReactElement = prettyFormat.plugins.ReactElement;
 const ReactTestComponent = prettyFormat.plugins.ReactTestComponent;
+const Immutable = prettyFormat.plugins.Immutable;
 
 
 module.exports = function stringify(obj) {
   if (typeof obj === "string")
       return obj;
   return prettyFormat(obj, {
-      plugins: [ReactElement, ReactTestComponent],
+      plugins: [ReactElement, ReactTestComponent, Immutable],
   });
 };
